bc6f78f7756e9b52f94b6471280dcb9c63e12c79
aloethyl <92891798+aloethyl@users.noreply.github.com>
2021-10-20 17:11:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Nick',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
@@ -25,7 +25,7 @@ const CONFIG = {
   // Weather
   weatherKey: 'InsertYourAPIKeyHere123456',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
+  weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
@@ -40,39 +40,33 @@ const CONFIG = {
   cards: [
     {
       id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://youtube.com',
     },
     {
       id: '2',
       name: 'Mail',
       icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      link: 'https://mail.google.com/mail/u/0/#inbox',
     },
     {
       id: '3',
-      name: 'Todoist',
+      name: 'High School',
       icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
+      link: 'https://idp.ncedcloud.org/idp/AuthnEngine#/authn',
     },
     {
       id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      name: 'College',
+      icon: 'trello',
+      link: 'https://idp.cpcc.edu/idp/profile/SAML2/Redirect/SSO?execution=e1s1',
     },
     {
       id: '5',
       name: 'Reddit',
       icon: 'bookmark',
       link: 'https://reddit.com',
-    },
-    {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
     },
   ],
 
@@ -81,46 +75,46 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+ // firstListIcon: 'music',
+ // secondListIcon: 'coffee',
 
   // Links
-  lists: {
-    firstList: [
-      {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      },
-      {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      },
-      {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      },
-      {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      },
-    ],
-    secondList: [
-      {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
-      },
-      {
-        name: 'Figma',
-        link: 'https://figma.com/',
-      },
-      {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
-      },
-      {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
-      },
-    ],
+ // lists: {
+   // firstList: [
+     // {
+       // name: 'Inspirational',
+       // link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+     // },
+     // {
+       // name: 'Classic',
+       // link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+     // },
+     // {
+       // name: 'Oldies',
+       // link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      //},
+      //{
+       // name: 'Rock',
+       // link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+     // },
+   // ],
+   // secondList: [
+     // {
+     //   name: 'Linkedin',
+      //  link: 'https://linkedin.com/',
+     // },
+     // {
+       // name: 'Figma',
+       // link: 'https://figma.com/',
+     // },
+     // {
+       // name: 'Dribbble',
+       // link: 'https://dribbble.com',
+     // },
+     // {
+       // name: 'Telegram',
+       // link: 'https://webk.telegram.org',
+     // },
+   // ],
   },
 };
